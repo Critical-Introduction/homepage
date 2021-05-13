903ca5576f7129a6bc3f4361362ae89f2c52cca2
James Dimonaco <dimonaco.james@gmail.com>
2021-05-13 21:55:11 +0100

    remove call to action

--- Navigation/components/Header.tsx
+++ Navigation/components/Header.tsx
@@ -44,10 +44,6 @@ const features = [
       icon: RefreshIcon,
     },
   ]
-  const callsToAction = [
-    { name: 'Watch Demo', href: '#', icon: PlayIcon },
-    { name: 'Contact Sales', href: '#', icon: PhoneIcon },
-  ]
   const resources = [
     {
       name: 'Help Center',
@@ -153,19 +149,6 @@ const Header = () => {
                                       <p className="mt-1 text-sm text-gray-500">{item.description}</p>
                                     </div>
                                   </a>
-                                ))}
-                              </div>
-                              <div className="px-5 py-5 bg-gray-50 space-y-6 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
-                                {callsToAction.map((item) => (
-                                  <div key={item.name} className="flow-root">
-                                    <a
-                                      href={item.href}
-                                      className="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-100"
-                                    >
-                                      <item.icon className="flex-shrink-0 h-6 w-6 text-gray-400" aria-hidden="true" />
-                                      <span className="ml-3">{item.name}</span>
-                                    </a>
-                                  </div>
                                 ))}
                               </div>
                             </div>
