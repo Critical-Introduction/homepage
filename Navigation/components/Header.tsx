4541fabfa149435e6585778c99903c2565ed0480
James Dimonaco <dimonaco.james@gmail.com>
2021-05-15 16:25:47 +0100

    change wording

--- Navigation/components/Header.tsx
+++ Navigation/components/Header.tsx
@@ -262,13 +262,13 @@ const Header = () => {
                 </Popover.Group>
                 <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
                   <a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
-                    Sign in
+                    Login
                   </a>
                   <a
                     href="#"
                     className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-red-600 hover:bg-red-700"
                   >
-                    Sign up
+                    Register
                   </a>
                 </div>
               </div>
@@ -345,12 +345,12 @@ const Header = () => {
                         href="#"
                         className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-red-600 hover:bg-red-700"
                       >
-                        Sign up
+                        Register
                       </a>
                       <p className="mt-6 text-center text-base font-medium text-gray-500">
                         Existing customer?
                         <a href="#" className="text-red-600 hover:text-red-500">
-                          Sign in
+                          Login
                         </a>
                       </p>
                     </div>
