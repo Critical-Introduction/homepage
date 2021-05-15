8f091a67b1973f8f17e75839cde36204ce79bfa6
James Dimonaco <dimonaco.james@gmail.com>
2021-05-15 12:06:39 +0100

    start of a contect page

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,20 +29,25 @@ export default function Home() {
             <div className="mt-10 sm:flex sm:justify-center lg:justify-start">
               <div className="rounded-md shadow">
                 <Link href="/problemBuilder">
+                  <a>
                 <div
                   className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10"
                 >
                   Problem Builder
                 </div>
+                </a>
                 </Link>
               </div>
               <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
-                <a
-                  href="#"
+                <Link href='contactUs'>
+                  <a>
+                <div
                   className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-red-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
                 >
                   Contact us
+                </div>
                 </a>
+                </Link>
               </div>
             </div>
           </div>
