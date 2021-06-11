4be924298f24545f34d78cb92d275604b499ef79
James Dimonaco <dimonaco.james@gmail.com>
2021-06-11 19:46:04 +0100

    add Next Links

--- Navigation/components/Header.tsx
+++ Navigation/components/Header.tsx
@@ -112,9 +112,11 @@ export const Header = () => {
                   </Popover.Button>
                 </div>
                 <Popover.Group as="nav" className="hidden md:flex space-x-10">
-                <a href="/" className="text-base font-medium text-gray-500 hover:text-gray-900">
+                  <Link href="/">
+                <a  className="text-base font-medium text-gray-500 hover:text-gray-900">
                     Home
                   </a>
+                  </Link>
                   <Popover className="relative">
                     {({ open }) => (
                       <>
@@ -151,9 +153,10 @@ export const Header = () => {
                             <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
                               <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
                               {features.map((item) => (
+                                <Link href={item.href}> 
                                   <a
                                     key={item.name}
-                                    href={item.href}
+                                    
                                     className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
                                   >
                                     <item.icon className="flex-shrink-0 h-6 w-6 text-red-600" aria-hidden="true" />
@@ -162,6 +165,7 @@ export const Header = () => {
                                       <p className="mt-1 text-sm text-gray-500">{item.description}</p>
                                     </div>
                                   </a>
+                                  </Link>
                                 ))}
                               </div>
                               <div className="px-5 py-5 bg-gray-50 space-y-6 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
@@ -268,9 +272,11 @@ export const Header = () => {
                   </Popover>
                 </Popover.Group>
                 <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-                  <a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
-                    Login
+                  <Link href="#">
+                  <a  className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+                    Sign In
                   </a>
+                  </Link>
                   <a
                     href="#"
                     className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-red-600 hover:bg-red-700"
@@ -316,44 +322,52 @@ export const Header = () => {
                     <div className="mt-6">
                       <nav className="grid gap-y-8">
                         {features.map((item) => (
+                          <Link href={item.href} >
                           <a
                             key={item.name}
-                            href={item.href}
+                            
                             className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
                           >
                             <item.icon className="flex-shrink-0 h-6 w-6 text-red-600" aria-hidden="true" />
                             <span className="ml-3 text-base font-medium text-gray-900">{item.name}</span>
                           </a>
+                          </Link>
                         ))}
                       </nav>
                     </div>
                   </div>
                   <div className="py-6 px-5 space-y-6">
                     <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-                      <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                      <Link href="#" >
+                      <a className="text-base font-medium text-gray-900 hover:text-gray-700">
                         Pricing
                       </a>
-
-                      <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                      </Link>
+                          <Link href="#">
+                      <a  className="text-base font-medium text-gray-900 hover:text-gray-700">
                         Docs
                       </a>
+                      </Link>
                       {resources.map((item) => (
+                        <Link href={item.href} >
                         <a
                           key={item.name}
-                          href={item.href}
                           className="text-base font-medium text-gray-900 hover:text-gray-700"
                         >
                           {item.name}
                         </a>
+                        </Link>
                       ))}
                     </div>
                     <div>
+                      <Link href="/register">
                       <a
-                        href="#"
+                        
                         className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-red-600 hover:bg-red-700"
                       >
                         Register
                       </a>
+                      </Link>
                       <p className="mt-6 text-center text-base font-medium text-gray-500">
                         Existing customer?
                         <a href="#" className="text-red-600 hover:text-red-500">
